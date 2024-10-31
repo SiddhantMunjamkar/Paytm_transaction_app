c6ac3b02bd37f3adf9ecfa39b9e014353cd7a89b
siddhant <siddhantmunjamkar23@gmail.com>
2024-10-31 16:40:05 +0530

    Your commit message

---
+++ apps/user-app/app/(dashboard)/p2ptransfer/page.tsx
@@ -0,0 +1,11 @@
+import { SendCard } from "../../../components/SendCard";
+
+
+export default function P2P() {
+  {
+    return  <div className=" w-full">
+   <SendCard/>
+      </div>
+    
+  }
+}
